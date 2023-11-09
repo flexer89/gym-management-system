bd99fb4efae4051562b135f8a1544c233b30078c
Eryk Stańdo <eryk.stando@gmail.com>
2023-11-09 23:34:14 +0100

    DB modifications

--- database/schema.sql
+++ database/schema.sql
@@ -16,9 +16,8 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  position VARCHAR(255) NOT NULL,
+  position ENUM('admin', 'employee', 'trainer', 'manager') NOT NULL,
   date_of_employment DATE NOT NULL,
-  is_manager BOOLEAN NOT NULL,
   PRIMARY KEY (id)
 );
 
