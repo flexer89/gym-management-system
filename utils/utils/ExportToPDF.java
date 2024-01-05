8f8cf6a36ae7e8ac02d8a89d2cf4f249446a0b11
Jakub Olszak <olsza.op@gmail.com>
2024-01-05 16:49:16 +0100

    Refactor ExportToPDF class to export table rows to multiple pages

--- utils/utils/ExportToPDF.java
+++ utils/utils/ExportToPDF.java
@@ -13,46 +13,48 @@ public class ExportToPDF {
     public static void ExportTableToPDF(DefaultTableModel model, String filePath) {
         try {
             PDDocument document = new PDDocument();
-            PDPage page = new PDPage();
-            document.addPage(page);
-
-            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                contentStream.beginText();
-                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 8);
-
-                int rowCount = model.getRowCount();
-                int colCount = model.getColumnCount();
-
-                float margin = 50;
-                float yStart = page.getMediaBox().getHeight() - margin;
-                float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
-                float yPosition = yStart;
-                float tableHeight = 20f;
-                float cellMargin = 2f;
-
-                // Write the table headers
-                contentStream.newLineAtOffset(margin, yPosition);
-                for (int i = 0; i < colCount; i++) {
-                    String header = model.getColumnName(i);
-                    contentStream.showText(header);
-                    contentStream.newLineAtOffset(tableWidth / colCount, 0);
-                }
+            int rowCount = model.getRowCount();
+            int colCount = model.getColumnCount();
+            int counter = 0;
+            
+            while (counter < rowCount) {
+                PDPage page = new PDPage();
+                document.addPage(page);
                 
-                // Write the table rows
-                for (int i = 0; i < rowCount; i++) {
-                    contentStream.newLineAtOffset(-tableWidth, -tableHeight);
-                    yPosition -= tableHeight;
-                    for (int j = 0; j < colCount; j++) {
-                        String cell = model.getValueAt(i, j).toString();
-                        // Fit the cell value to prevent overflow
-                        float cellWidth = tableWidth / colCount - 2 * cellMargin;
-                        String fittedCell = fitCellValue(cell, cellWidth, contentStream);
-                        contentStream.showText(fittedCell);
+                try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                    float margin = 50;
+                    float yStart = page.getMediaBox().getHeight() - margin;
+                    float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
+                    float yPosition = yStart;
+                    float tableHeight = 20f;
+                    float cellMargin = 2f;
+                    contentStream.beginText();
+                    contentStream.setFont(PDType1Font.HELVETICA_BOLD, 8);
+
+                    // Write the table headers
+                    contentStream.newLineAtOffset(margin, yPosition);
+                    for (int i = 0; i < colCount; i++) {
+                        String header = model.getColumnName(i);
+                        contentStream.showText(header);
                         contentStream.newLineAtOffset(tableWidth / colCount, 0);
                     }
+                    
+                    // Write the table rows
+                    while (counter < rowCount && yPosition > margin) {
+                        contentStream.newLineAtOffset(-tableWidth, -tableHeight);
+                        yPosition -= tableHeight;
+                        for (int j = 0; j < colCount; j++) {
+                            String cell = model.getValueAt(counter, j).toString();
+                            // Fit the cell value to prevent overflow
+                            float cellWidth = tableWidth / colCount - 2 * cellMargin;
+                            String fittedCell = fitCellValue(cell, cellWidth, contentStream);
+                            contentStream.showText(fittedCell);
+                            contentStream.newLineAtOffset(tableWidth / colCount, 0);
+                        }
+                        counter++;
+                    }
+                    contentStream.endText();
                 }
-
-                contentStream.endText();
             }
 
             document.save(filePath);
